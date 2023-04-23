29b90ee70955c5ba96ed22ddb69b924cc8e25a71
marie <marie_maunoury@hotmail.fr>
2023-04-23 02:20:40 +0200

    Create french receipes + clean the card view

--- src/views/ReceipesView.vue
+++ src/views/ReceipesView.vue
@@ -1,29 +1,46 @@
 <template>
-    <ion-page>
-      <ion-header>
-        <ion-toolbar>
-          <ion-title>Mes recettes</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-content :fullscreen="true">
-        <p>Le café s'invite en cuisine</p>
-      </ion-content>
-    </ion-page>
-  </template>
-  
-  <script>
-  import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title type="ios">Mes recettes</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    
+    <ion-content :fullscreen="true">
+      <ion-card :button="true" v-for="receipe in receipes" :key="receipe.id">
+        <img alt="Receipe's image" :src=receipe.image />
+        <ion-card-header>
+          <ion-card-title>{{ receipe.title }}</ion-card-title>
+        </ion-card-header>
+        <ion-card-content>{{ receipe.description }}</ion-card-content>
+      </ion-card>
+    </ion-content>
+  </ion-page>
+</template>
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/vue';
+import { ref } from 'vue'
+import receipesData from '../receipes.json'
+
 export default {
-    name: 'ReceipesView',
-    components: {
-        IonPage,
-        IonHeader,
-        IonToolbar,
-        IonTitle,
-        IonContent,
-    },
-    setup() {
+  name: 'RecipesView',
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardTitle
+  },
+  setup() {
+    const receipes = ref(receipesData)
+    
+    return {
+      receipes
     }
-}
-  </script>
-  
+  }
+};
+</script>
